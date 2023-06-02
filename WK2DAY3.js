cdffe0d6d3b376f7531bddb2b2cf411a3a3aa559
PerScholas Student <areyonaiveyihs@gmail.com>
2023-06-02 14:50:02 -0400

    first commit

---
+++ WK2DAY3.js
@@ -0,0 +1,18 @@
+let student=60;
+
+if (student>=80){
+    console.log("You did a good job")
+}
+else if(student>=70 && student< 80){
+        console.log("Keep Trying")
+}
+else if(student>=60 && student< 70){
+        console.log("Ehhhh")
+}
+else if(student>=55 && student< 60){
+        console.log("Not to good")
+}
+else{
+        console.log("Bad Grade But You'll Get Em Next Time")
+     }
+
---
+++ index.js
@@ -0,0 +1,54 @@
+//let x=15;
+//console.log(x); 
+//let a=7;
+//console.log(a);
+//let names=["john","jim","lucy"]
+//console.log(names);
+
+//let x= 12;
+//let a= 78;
+//if (x===12 && a===78) {
+//    let sum=x+a;
+//console.log(sum);
+//};
+
+/*let wood=23;
+if (wood>40){
+    console.log("You can build a tree house!");
+}
+else if (wood>20 && wood<40){
+    console.log("build a table")
+
+}
+else{
+    console.log("Its a no go");
+}
+
+
+
+
+
+
+let username="Admin";
+let password="Pass614";
+
+if (username==="Admin" && password==="Pass614"){
+console.log("Access Granted");
+} 
+else{
+    console.log("Access denied");
+}*/
+
+let num=0;
+
+if (num>0 && num>=100){
+    console.log("good");
+}
+
+else if(num >0 && num<100){
+    console.log("alright");
+}
+
+else{
+    console.log("bad");
+}
